e45aff9d3d26d18751baa6e1e46a4eb5ecea9605
sarajose <sarajose6@gmail.com>
2023-01-09 17:36:37 +0100

    load_data() added

--- code/main.py
+++ code/main.py
@@ -5,10 +5,25 @@ Created on Mon Jan 09, 2023
 
 """
 
+import os
+import sys
 import pandas as pd
 
+def load_data():
+    
+    # Create path for the dataset
+    script_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+    project_root_path = os.path.dirname(script_path)
+    dataset_path = os.path.join(project_root_path, "data/shelters_and_campsites.csv")    
+    print("Loading dataset from: " + dataset_path) 
+    
+    # Load data
+    df = pd.read_csv(dataset_path)
+    return df
+
 def main():
-    pass
+    
+    df = load_data()
 
 if __name__ == "__main__":
     main()
