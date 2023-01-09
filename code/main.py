80b6fcb913600fe20cded0c9bed5a1bbe43633ad
sarajose <sarajose6@gmail.com>
2023-01-09 18:22:58 +0100

    load_data() moved to clean_data module

--- code/main.py
+++ code/main.py
@@ -5,25 +5,17 @@ Created on Mon Jan 09, 2023
 
 """
 
-import os
-import sys
-import pandas as pd
+import clean_data
 
-def load_data():
-    
-    # Create path for the dataset
-    script_path = os.path.dirname(os.path.abspath(sys.argv[0]))
-    project_root_path = os.path.dirname(script_path)
-    dataset_path = os.path.join(project_root_path, "data/shelters_and_campsites.csv")    
-    print("Loading dataset from: " + dataset_path) 
-    
-    # Load data
-    df = pd.read_csv(dataset_path)
-    return df
 
-def main():
+def main():    
+    df = clean_data.load_data()
     
-    df = load_data()
+    # Select data
+    # Clean data
+    # Data analysis
+    
+    # Graphs
 
 if __name__ == "__main__":
     main()
