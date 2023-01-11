f7ac7ddd5326bfcc80b508f7a0fee42272180898
Joan <jperacaula1@gmail.com>
2023-01-11 20:06:03 +0100

    improve and add data integration process to the main script

--- code/clean_data.py
+++ code/clean_data.py
@@ -10,17 +10,7 @@ import os
 import pandas as pd
 import numpy as np
     
-def load_data():
-    
-    # Create path for the dataset
-    script_path = os.path.dirname(os.path.abspath(sys.argv[0]))
-    project_root_path = os.path.dirname(script_path)
-    dataset_path = os.path.join(project_root_path, "data/shelters_and_campsites.csv")    
-    print("Loading dataset from: " + dataset_path) 
-    
-    # Load data
-    df = pd.read_csv(dataset_path)
-    return df
+
 
 def clean_data(df):
     # Dataset information
--- code/main.py
+++ code/main.py
@@ -4,19 +4,36 @@ Created on Mon Jan 09, 2023
 @author: Sara Jose, Joan Peracaula
 
 """
-
+import os 
+import sys
+import pandas as pd
 import clean_data
+from data_integration import create_custom_DEM, integrate_elevations
+
+
+# Set constant paths
+SCRIPT_PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
+PROJECT_ROOT_PATH = os.path.dirname(SCRIPT_PATH)
+INITIAL_DATASET_PATH = os.path.join(PROJECT_ROOT_PATH, "data/initial_shelters_and_campsites.csv")
+FINAL_DATASET_PATH = os.path.join(PROJECT_ROOT_PATH, "data/final_shelters_and_campsites.csv")
+
 
 def main():  
-    # Load dataser
-    df = clean_data.load_data()
+
+    # Load initial dataset
+    df = pd.read_csv(INITIAL_DATASET_PATH)
     
     # Select and clean data
-    cleaned_df = clean_data.clean_data(df)
+    df = clean_data.clean_data(df)
+
+    # Data integration
+    create_custom_DEM() # Create a custom DEM of the geographical area of the dataset
+    final_df = integrate_elevations(df) # Fill empty altitudes using the DEM and the data coords
     
-    # Data analysis
-    
-    # Graphs
+    # Save the cleaned and preprocessed dataset, ready to analyse
+    final_df.to_csv(FINAL_DATASET_PATH)
+
 
 if __name__ == "__main__":
     main()
+    
